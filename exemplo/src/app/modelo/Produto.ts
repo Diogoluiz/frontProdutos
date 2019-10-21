e10a6e13559c828415ee44c552f7e1b7675b98d0
Diogoluiz <diogosteidel@hotmail.com>
2019-10-21 20:47:01 -0300

    Enviando o front-end

---
+++ exemplo/src/app/modelo/Produto.ts
@@ -0,0 +1,8 @@
+export class Produto{
+
+    // Atributos
+    public codigo : number;
+    public produto : string;
+    public marca : string;
+    public valor : number;
+}
---
+++ exemplo/src/app/produto/produto.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Produto } from '../modelo/produto';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Mensagem } from '../modelo/mensagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  // Url
+  private url = "http://localhost:8085/api/produto";
+
+  // Construtor
+  constructor(private http:HttpClient) { }
+    
+    // Listar todos os produtos
+    listarProdutos():Observable<Produto[]>{
+      return this.http.get<Produto[]>(this.url);
+    }
+  
+  // Cadastrar um novo produto
+  cadastrarProduto(objeto : Produto) :Observable<Mensagem>{
+    return this.http.post<Mensagem>(this.url, objeto);
+  }
+
+  // Alterar um produto
+  alterarProduto(objeto: Produto):Observable<Mensagem>{
+    return this.http.put<Mensagem>(this.url, objeto);
+  }
+
+  // Excluir um produto
+  excluirProduto(codigo:number):Observable<Mensagem>{
+    return this.http.delete<Mensagem>(this.url+"/"+codigo);
+  }
+}
